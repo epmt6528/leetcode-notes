e864357509f138e5ff84bfe3e570b16aa03b030c
epmt6528 <epmt6528@gmail.com>
2023-07-04 16:48:12 -0700

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ 235_lowest_common_ancestor_of_a_binary_search_tree /20230704.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+// Recursive
+var lowestCommonAncestor = function (root, p, q) {
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
+
+// Iterative
+var lowestCommonAncestor = function (root, p, q) {
+  while (root) {
+    if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else {
+      return root;
+    }
+  }
+  return null;
+};
