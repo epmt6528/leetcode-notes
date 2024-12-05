11f1b5e0457d1598d98d0d524c413d0a4603f963
epmt6528 <epmt6528@gmail.com>
2024-12-04 19:52:10 -0800

    3. Longest Substring Without Repeating Characters (other solution)

--- 3_longest_substring_without_repeating_characters/20241204.js
+++ 3_longest_substring_without_repeating_characters/20241204.js
@@ -49,3 +49,20 @@ var lengthOfLongestSubstring = function(s) {
 
   return longestLength
 };
+
+function lengthOfLongestSubstring(s) {
+  let charSet = new Set();
+  let start = 0;
+  let maxLength = 0;
+
+  for (let end = 0; end < s.length; end++) {
+    while (charSet.has(s[end])) {
+      charSet.delete(s[start]);
+      start++;
+    }
+    charSet.add(s[end]);
+    maxLength = Math.max(maxLength, end - start + 1);
+  }
+
+  return maxLength;
+}
